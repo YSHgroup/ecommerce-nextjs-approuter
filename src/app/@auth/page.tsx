145258b8189ab5ce62b77e03ec10ec21de25347a
YSHgroup <david.dev100@outlook.com>
2023-10-22 18:20:40 -0700

    auth route group created and @page file deleted

--- src/app/@auth/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div>
-      <div className='prose prose-sm prose-invert max-w-none'>
-        <h2 className='text-lg font-bold'>Auth</h2>
-      </div>
-    </div>
-  )
-}
