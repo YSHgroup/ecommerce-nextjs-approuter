85d56445e3f9766aa9c11e630fcb44311149c9d1
YSHgroup <david.dev100@outlook.com>
2023-10-22 23:53:51 -0700

    create carousel file and route handler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main className='flex h-screen flex-col items-center justify-between'>
       <div className='background h-screen w-full '></div>
-              <Image src={"https://i.dummyjson.com/data/products/1/1.jpg"} fill className="object-cover" alt="alt" />
       <Content/>
     </main>
   )
